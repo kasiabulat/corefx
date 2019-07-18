38e0bbb870f5185735db55132b14fff4ec9368d0
Katarzyna Bulat <t-kabul@microsoft.com>
2019-07-18 15:44:26 -0700

    asserts added to the first half of tests file

--- src/System.Text.Json/tests/WritableJsonApiTests.cs
+++ src/System.Text.Json/tests/WritableJsonApiTests.cs
@@ -4,6 +4,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text.Json.Serialization.Tests;
 using Xunit;
@@ -150,6 +151,20 @@ namespace System.Text.Json
                 { "is developer", true },
                 { "null property", (JsonNode) null }
             };
+
+            Assert.IsType<JsonString>(developer["name"]);
+            var developerNameCasted = developer["name"] as JsonString;
+            Assert.Equal("Kasia", developerNameCasted.Value);
+
+            Assert.IsType<JsonNumber>(developer["age"]);
+            var developerAgeCasted = developer["age"] as JsonNumber;
+            Assert.Equal(22, developerAgeCasted.GetInt32());
+
+            Assert.IsType<JsonBoolean>(developer["is developer"]);
+            var isDeveloperCasted = developer["is developer"] as JsonBoolean;
+            Assert.Equal(true, isDeveloperCasted.Value);
+
+            Assert.Null(developer["null property"]);
         }
 
         /// <summary>
@@ -164,6 +179,18 @@ namespace System.Text.Json
                 { "age", new JsonNumber(22) },
                 { "is developer", new JsonBoolean(true) }
             };
+
+            Assert.IsType<JsonString>(developer["name"]);
+            var developerNameCasted = developer["name"] as JsonString;
+            Assert.Equal("Kasia", developerNameCasted.Value);
+
+            Assert.IsType<JsonNumber>(developer["age"]);
+            var developerAgeCasted = developer["age"] as JsonNumber;
+            Assert.Equal(22, developerAgeCasted.GetInt32());
+
+            Assert.IsType<JsonBoolean>(developer["is developer"]);
+            var isDeveloperCasted = developer["is developer"] as JsonBoolean;
+            Assert.Equal(true, isDeveloperCasted.Value);
         }
 
         /// <summary>
@@ -180,11 +207,27 @@ namespace System.Text.Json
                 { "area", PI }
             };
 
-            JsonNumber bigConstantBoxed = Int64.MaxValue;
+            Assert.IsType<JsonNumber>(circle["radius"]);
+            var radius = circle["radius"] as JsonNumber;
+            Assert.Equal(radius, 1);
+
+            Assert.IsType<JsonNumber>(circle["length"]);
+            var length = circle["length"] as JsonNumber;
+            Assert.Equal(length, 2 * PI);
+
+            Assert.IsType<JsonNumber>(circle["area"]);
+            var area = circle["area"] as JsonNumber;
+            Assert.Equal(area, PI);
+
+            JsonNumber bigConstantBoxed = long.MaxValue;
             long bigConstant = bigConstantBoxed.GetInt64();
+
+            Assert.Equal(long.MaxValue, bigConstant);
 
             var smallValueBoxed = new JsonNumber(17);
             smallValueBoxed.TryGetInt16(out short smallValue);
+
+            Assert.Equal(17, smallValue);
         }
 
         /// <summary>
@@ -229,6 +272,16 @@ namespace System.Text.Json
                     }
                 }
             };
+
+            Assert.IsType<JsonObject>(person["phone numbers"]);
+            var phoneNumbers = person["phone numbers"] as JsonObject;
+            Assert.IsType<JsonString>(phoneNumbers["work"]);
+            Assert.IsType<JsonString>(phoneNumbers["home"]);
+
+            Assert.IsType<JsonObject>(person["addresses"]);
+            var addresses = person["office"] as JsonObject;
+            Assert.IsType<JsonObject>(addresses["office"]);
+            Assert.IsType<JsonObject>(addresses["home"]);
         }
 
         /// <summary>
@@ -238,6 +291,7 @@ namespace System.Text.Json
         public static void TestAssignmentDefinition()
         {
             JsonNode employee = EmployeesDatabase.GetNextEmployee().Value;
+            Assert.IsType<JsonObject>(employee);
         }
 
         /// <summary>
@@ -253,6 +307,15 @@ namespace System.Text.Json
                 EmployeesDatabase.GetNextEmployee(),
                 EmployeesDatabase.GetNextEmployee(),
             };
+
+            string prevId = "";
+            foreach((string id, JsonNode employee) in employees)
+            {
+                Assert.NotEqual(prevId, id);
+                prevId = id;
+
+                Assert.IsType<JsonObject>(employee);
+            }
         }
 
         /// <summary>
@@ -266,6 +329,15 @@ namespace System.Text.Json
             {
                 employees.Add(employee);
             }
+
+            string prevId = "";
+            foreach ((string id, JsonNode employee) in employees)
+            {
+                Assert.NotEqual(prevId, id);
+                prevId = id;
+
+                Assert.IsType<JsonObject>(employee);
+            }
         }
 
         /// <summary>
@@ -275,6 +347,15 @@ namespace System.Text.Json
         public static void TestAddingKeyValuePairsCollection()
         {
             var employees = new JsonObject(EmployeesDatabase.GetTenBestEmployees());
+
+            string prevId = "";
+            foreach ((string id, JsonNode employee) in employees)
+            {
+                Assert.NotEqual(prevId, id);
+                prevId = id;
+
+                Assert.IsType<JsonObject>(employee);
+            }
         }
 
         /// <summary>
@@ -285,16 +366,94 @@ namespace System.Text.Json
         {
             var employees = new JsonObject();
             employees.AddRange(EmployeesDatabase.GetTenBestEmployees());
+
+            string prevId = "";
+            foreach ((string id, JsonNode employee) in employees)
+            {
+                Assert.NotEqual(prevId, id);
+                prevId = id;
+
+                Assert.IsType<JsonObject>(employee);
+            }
         }
 
         /// <summary>
-        /// Creating Json array
+        /// Adding JsonArray to JsonObject by creating it in initializing collection
         /// </summary>
         [Fact]
-        public static void TestCreatingJsonArray()
+        public static void TestAddingJsonArray()
         {
-            string[] dishes = { "sushi", "pasta", "cucumber soup" };
+            var preferences = new JsonObject()
+                {
+                    { "colours", new JsonArray { "red", "green", "purple" } }       
+                };
 
+            Assert.IsType<JsonArray>(preferences["colours"]);
+            var colours = preferences["colours"] as JsonArray;
+            Assert.Equal(3, colours.Count);
+
+            string[] expected = { "red", "green", "blue" };
+
+            for (int i = 0; i < colours.Count; i++)
+            {
+                Assert.IsType<JsonString>(colours[i]);
+                Assert.Equal(expected[i], colours[i] as JsonString);
+            }
+        }
+
+        /// <summary>
+        /// Adding JsonArray to JsonObject by creating it from string array
+        /// </summary>
+        [Fact]
+        public static void TestCretingJsonArrayFromStringArray()
+        {
+            string[] expected = { "sushi", "pasta", "cucumber soup" };
+            var preferences = new JsonObject()
+                {
+                    { "dishes", new JsonArray(expected) }
+                };
+
+            Assert.IsType<JsonArray>(preferences["dishes"]);
+            var dishesJson = preferences["dishes"] as JsonArray;
+            Assert.Equal(3, dishesJson.Count);
+
+            for (int i = 0; i < dishesJson.Count; i++)
+            {
+                Assert.IsType<JsonString>(dishesJson[i]);
+                Assert.Equal(expected[i], dishesJson[i] as JsonString);
+            }
+        }
+
+        /// <summary>
+        /// Adding JsonArray to JsonObject by passing JsonNumber array
+        /// </summary>
+        [Fact]
+        public static void TestAddingJsonArrayFromJsonNumberArray()
+        {
+            var preferences = new JsonObject()
+                {
+                     { "prime numbers", new JsonNumber[] { 19, 37 } }
+                };
+
+            Assert.IsType<JsonArray>(preferences["prime numbers"]);
+            var primeNumbers = preferences["prime numbers"] as JsonArray;
+            Assert.Equal(2, primeNumbers.Count);
+
+            int[] expected = { 19, 37 };
+
+            for (int i = 0; i < primeNumbers.Count; i++)
+            {
+                Assert.IsType<JsonNumber>(primeNumbers[i]);
+                Assert.Equal(expected[i], primeNumbers[i] as JsonNumber);
+            }
+        }
+
+        /// <summary>
+        /// Adding JsonArray to JsonObject by passing IEnumerable of strings
+        /// </summary>
+        [Fact]
+        public static void TestAddingJsonArrayFromIEnumerableOfStrings()
+        {
             var sportsExperienceYears = new JsonObject()
             {
                 { "skiing", 5 },
@@ -307,6 +466,28 @@ namespace System.Text.Json
             // choose only sports with > 2 experience years
             IEnumerable<string> sports = sportsExperienceYears.Where(sport => ((JsonNumber)sport.Value).GetInt32() > 2).Select(sport => sport.Key);
 
+            var preferences = new JsonObject()
+                {
+                     { "sports", new JsonArray(sports) }
+                };
+
+            Assert.IsType<JsonArray>(preferences["sports"]);
+            var sportsJsonArray = preferences["sports"] as JsonArray;
+            Assert.Equal(3, sportsJsonArray.Count);
+
+            for (int i = 0; i < sportsJsonArray.Count; i++)
+            {
+                Assert.IsType<JsonString>(sportsJsonArray[i]);
+                Assert.Equal(sports.ElementAt(i), sportsJsonArray[i] as JsonString);
+            }
+        }
+
+        /// <summary>
+        /// Adding JsonArray to JsonObject by passing IEnumerable of JsonNodes
+        /// </summary>
+        [Fact]
+        public static void TestAddingJsonArrayFromIEnumerableOfJsonNodes()
+        {
             var strangeWords = new JsonArray()
             {
                 "supercalifragilisticexpialidocious",
@@ -316,15 +497,21 @@ namespace System.Text.Json
             };
 
             var preferences = new JsonObject()
+                {
+                     { "strange words", strangeWords.Where(word => ((JsonString)word).Value.Length < 10) }
+                };
+
+            Assert.IsType<JsonArray>(preferences["strange words"]);
+            var strangeWordsJsonArray = preferences["strange words"] as JsonArray;
+            Assert.Equal(2, strangeWordsJsonArray.Count);
+
+            string [] expected = { "gladiolus", "albumen" };
+
+            for (int i = 0; i < strangeWordsJsonArray.Count; i++)
             {
-                { "colours", new JsonArray { "red", "green", "purple" } },
-                { "numbers", new JsonArray { 4, 123, 88 } },
-                { "prime numbers", new JsonNumber[] { 19, 37 } },
-                { "varia", new JsonArray { 17, "green", true } },
-                { "dishes", new JsonArray(dishes) },
-                { "sports", new JsonArray(sports) },
-                { "strange words", strangeWords.Where(word => ((JsonString)word).Value.Length < 10) },
-            };
+                Assert.IsType<JsonString>(strangeWordsJsonArray[i]);
+                Assert.Equal(expected[i], strangeWordsJsonArray[i] as JsonString);
+            }
         }
 
         /// <summary>
@@ -362,6 +549,12 @@ namespace System.Text.Json
                     }
                 },
             };
+
+            Assert.IsType<JsonArray>(vertices[0]);
+            var innerJsonArray = vertices[0] as JsonArray;
+            Assert.IsType<JsonArray>(innerJsonArray[0]);
+            innerJsonArray = innerJsonArray[0] as JsonArray;
+            Assert.IsType<JsonArray>(innerJsonArray[0]);
         }
 
         /// <summary>
@@ -376,6 +569,15 @@ namespace System.Text.Json
             {
                 employeesIds.Add(employee.Key);
             }
+
+            JsonString prevId = new JsonString();
+            foreach(JsonNode employeeId in employeesIds)
+            {
+                Assert.IsType<JsonString>(employeeId);
+                var employeeIdString = employeeId as JsonString;
+                Assert.NotEqual(prevId, employeeIdString);
+                prevId = employeeIdString;
+            }
         }
 
         /// <summary>
@@ -385,6 +587,15 @@ namespace System.Text.Json
         public static void TestCreatingJsonArrayFromCollection()
         {
             var employeesIds = new JsonArray(EmployeesDatabase.GetTenBestEmployees().Select(employee => new JsonString(employee.Key)));
+
+            JsonString prevId = new JsonString();
+            foreach (JsonNode employeeId in employeesIds)
+            {
+                Assert.IsType<JsonString>(employeeId);
+                var employeeIdString = employeeId as JsonString;
+                Assert.NotEqual(prevId, employeeIdString);
+                prevId = employeeIdString;
+            }
         }
 
         /// <summary>
@@ -394,6 +605,15 @@ namespace System.Text.Json
         public static void TestCreatingJsonArrayFromCollectionOfString()
         {
             var employeesIds = new JsonArray(EmployeesDatabase.GetTenBestEmployees().Select(employee => employee.Key));
+
+            JsonString prevId = new JsonString();
+            foreach (JsonNode employeeId in employeesIds)
+            {
+                Assert.IsType<JsonString>(employeeId);
+                var employeeIdString = employeeId as JsonString;
+                Assert.NotEqual(prevId, employeeIdString);
+                prevId = employeeIdString;
+            }
         }
 
         /// <summary>
@@ -412,6 +632,11 @@ namespace System.Text.Json
             {
                 EmployeesDatabase.CheckSSN(((JsonString)person["ssn"]).Value);
             }
+
+            Assert.True(person.ContainsProperty("ssn"));
+            Assert.False(person.ContainsProperty("surname"));
+
+            // Different scenario:
 
             var enabledOptions = new JsonArray
             {
@@ -454,12 +679,21 @@ namespace System.Text.Json
             // Assign by creating a new instance of primary Json type
             person1["name"] = new JsonString("Bob");
 
+            Assert.IsType<JsonString>(person1["name"]);
+            Assert.Equal(person1["name"] as JsonString, "Bob");
+
             // Assign by using an implicit operator on primary Json type
             JsonNumber newAge = 55;
             person1["age"] = newAge;
 
+            Assert.IsType<JsonNumber>(person1["age"]);
+            Assert.Equal(person1["age"] as JsonNumber, 55);
+
             // Assign by explicit cast from Json primary type
-            person1["is_married"] = (JsonBoolean)true;
+            person1["is_married"] = (JsonBoolean)false;
+
+            Assert.IsType<JsonBoolean>(person1["is_married"]);
+            Assert.Equal(person1["is_married"] as JsonBoolean, false);
 
             // Not possible scenario (wold require implicit cast operators in JsonNode):
             // person["name"] = "Bob";
@@ -474,8 +708,13 @@ namespace System.Text.Json
             // Copy property from another JsonObject
             person1["age"] = person2["age"];
 
+            Assert.IsType<JsonNumber>(person1["age"]);
+            Assert.Equal(person1["age"] as JsonNumber, 33);
+
             // Copy property of different typoe
-            person1["name"] = person2["name"];         
+            person1["name"] = person2["name"];
+
+            Assert.IsType<JsonArray>(person1["name"]);
         }
 
         /// <summary>
@@ -487,20 +726,31 @@ namespace System.Text.Json
             JsonString name = "previous name";
             name.Value = "new name";
 
+            Assert.Equal("new name", name.Value);
+
             bool shouldBeEnabled = true;
             var isEnabled = new JsonBoolean(false);
             isEnabled.Value = shouldBeEnabled;
 
+            Assert.True(isEnabled.Value);
+
             JsonNumber veryBigConstant = new JsonNumber();
             veryBigConstant.SetString("1e1000");
             string bigNumber = veryBigConstant.GetString();
+
+            Assert.Equal("1e1000", bigNumber);
+
             veryBigConstant.SetInt16(123);
             short smallNumber = veryBigConstant.GetInt16();
+
+            Assert.Equal(123, smallNumber);
 
             // Incrementing age:
             JsonObject employee = EmployeesDatabase.GetManager();
             int age = ((JsonNumber)employee["age"]).GetInt32();
             ((JsonNumber)employee["age"]).SetInt32(age + 1);
+
+            Assert.Equal(46, ((JsonNumber)employee["age"]).GetInt32());
         }
 
         /// <summary>
